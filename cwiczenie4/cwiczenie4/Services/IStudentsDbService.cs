05f46324b137971e91bf05e9cccfa640681cf7bf
s16522 <s16522@pjwstk.edu.pl>
2020-05-08 08:06:05 +0200

    cw 5

---
+++ cwiczenie4/cwiczenie4/Services/IStudentsDbService.cs
@@ -0,0 +1,11 @@
+﻿using cwiczenie3.DTO;
+
+namespace cwiczenie3.Services
+{
+    public interface IStudentsDbService
+    {
+        public string PostStudent(PostStudentRequest student);
+
+        public string PromoteStudents(PromoteStudentRequest request);
+    }
+}
